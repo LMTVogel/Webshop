fc8ec7fadeea9364267f4fe4fed18c5038bbd0e5
Luuk Vogel <99051050@mydavinci.nl>
2020-08-23 23:54:43 +0200

    Created evreything to seed the category table

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Category::insert([
+            'name' => 'Goocheltrucs',
+            'image' => 'images/goocheltrucs.jpeg',
+        ]);
+
+        \App\Category::insert([
+            'name' => "DVD's",
+            'image' => 'images/dvd.jpg',
+        ]);
+
+        \App\Category::insert([
+            'name' => 'Boeken',
+            'image' => 'images/boeken.jpg',
+        ]);
+
+        \App\Category::insert([
+            'name' => 'Accessoires',
+            'image' => 'images/accessoires.jpg',
+        ]);
+
+        \App\Category::insert([
+            'name' => 'Kaarten',
+            'image' => 'images/kaarten.jpg',
+        ]);
+    }
+}
