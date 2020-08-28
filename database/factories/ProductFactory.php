ae50d4d30d06d38c7c293f13d894a2255d8a1603
Luuk Vogel <99051050@mydavinci.nl>
2020-08-28 19:59:28 +0200

    Made a factory for the products.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->word,
+        'description' => $faker->sentence($nbWords = 20, $variableNbWords = true),
+        'price' => rand(10, 50),
+        'image' => 'images/kaarten.jpg',
+        'cat_id' => rand(1, 5),
     ];
 });
