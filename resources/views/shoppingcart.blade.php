d70e1f32dd59aab15e79284bc091dd80af88db6d
Luuk Vogel <99051050@mydavinci.nl>
2020-09-24 13:55:23 +0200

    You can now increase item inside cart

--- resources/views/shoppingcart.blade.php
+++ resources/views/shoppingcart.blade.php
@@ -3,17 +3,31 @@
 @section('content')
     @if (Session::has('cart'))
         <div class="row">
-            <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+            <div class="col-sm-6 col-md-offset-3 col-sm-offset-3">
                 <ul class="list-group">
                     @foreach ($products as $product)
                         <li class="list-group-item">
                             <strong>Product: {{ $product['item'] ['name'] }}</strong>
                             <span class="badge">Aantal producten: {{ $product['qty'] }}</span>
                             <span class="label label-success">{{ $product['price'] }},-</span>
+                            <div class="btn-group cartBtnGroup">
+                                <a class="btn qtyBtn rounded bg-success" href="{{ route('cart.addToCart', ['id' => $product['item']['id']]) }}"><i class="far fa-plus-square iconColor"></i></a>
+                                <a class="btn qtyBtn rounded bg-warning" href="{{ route('cart.addToCart', ['id' => $product['item']['id']]) }}"><i class="far fa-minus-square iconColor"></i></a>
+                                <a class="btn qtyBtn rounded bg-danger" href="{{ route('cart.addToCart', ['id' => $product['item']['id']]) }}"><i class="far fa-trash-alt iconColor"></i></a>
+                            </div>
                         </li>
                     @endforeach
                 </ul>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-offset-3 col-sm-offset-3">
+                <strong>Totaal prijs: {{ $totalPrice }},-</strong>
+            </div>
+            <hr>
+            {{-- <div class="col-sm-6 col-md-offset-3 col-sm-offset-3">
+                <strong>Totaal aantal producten: {{ $totalQty }},-</strong>
+            </div> --}}
         </div>
     @else 
         <p>Er zit nog niks in je winkelmandje!</p>
